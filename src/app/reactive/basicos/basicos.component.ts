f583e42bfe77a80934b1c024342fe762e86bdf90
Pablo Rodriguez <pablorodriguez88@gmail.com>
2022-04-10 18:05:59 -0300

    rective forms

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent {
+  miFormulario: FormGroup = new FormGroup({
+    nombre: new FormControl('RTX 4080ti'),
+  });
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
 }
