9692042f692a490d5f137119cdfa3f46ec260e61
Pablo Rodriguez <pablorodriguez88@gmail.com>
2022-04-10 17:39:44 -0300

    radio,check and switches

--- src/app/template/switches/switches.component.ts
+++ src/app/template/switches/switches.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class SwitchesComponent implements OnInit {
+export class SwitchesComponent {
+  persona = {
+    genero: '',
+    notificaciones: true,
+  };
 
-  constructor() { }
+  terminos = false;
 
-  ngOnInit(): void {
+  guardar() {
+    console.log(this.persona);
   }
-
 }
