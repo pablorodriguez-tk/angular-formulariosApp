79f632b6d77bc8646465d61eaa4cef783d2b5c60
Pablo Rodriguez <pablorodriguez88@gmail.com>
2022-04-10 16:21:43 -0300

    add elements on dinamic form

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,14 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+interface Favorito {
+  id: number;
+  nombre: string;
+}
 
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styles: [],
 })
-export class DinamicosComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class DinamicosComponent {
+  persona: Persona = {
+    nombre: 'Juan',
+    favoritos: [
+      { id: 1, nombre: 'Mario' },
+      { id: 2, nombre: 'Sonic' },
+      { id: 3, nombre: 'Mortal Kombat' },
+    ],
+  };
 
   guardar() {
     console.log('guardar');
